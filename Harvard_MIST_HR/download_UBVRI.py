99bf38013b96bcb28feb2decc5817a456044e5cf
Joshua Andres Grajales <92562825+jgrajales21@users.noreply.github.com>
2021-12-29 14:28:22 -0500

    Create download_UBVRI.py
    Source: http://waps.cfa.harvard.edu/MIST/model_grids.html

---
+++ Harvard_MIST_HR/download_UBVRI.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+import time
+import os
+
+s = Service("/usr/local/bin/chromedriver")
+
+def isochrone_file():
+    driver = webdriver.Chrome(service=s)
+    driver.get("http://waps.cfa.harvard.edu/MIST/model_grids.html")
+    driver.maximize_window()
+    driver.find_element_by_link_text("UBV(RI)c + 2MASS + Kepler + Hipparcos + Tycho + Gaia (114MB)").click()
+
+isochrone_file()
